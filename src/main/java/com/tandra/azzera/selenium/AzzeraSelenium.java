d2993066ddc43e94a2cd37dc2eeee1f0414c440c
waltassah@tandratech.com <waltassah@tandratech.com>
2022-10-11 11:49:52 +0300

    new files

--- src/main/java/com/tandra/azzera/selenium/AzzeraSelenium.java
+++ src/main/java/com/tandra/azzera/selenium/AzzeraSelenium.java
@@ -5,13 +5,69 @@
 
 package com.tandra.azzera.selenium;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 /**
  *
  * @author Waltassah
  */
 public class AzzeraSelenium {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws MalformedURLException {
+ 
+        String nodeURL="http://ec2-3-83-145-251.compute-1.amazonaws.com:4444";
+        
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setBrowserName("chrome");
+        cap.setPlatform(Platform.LINUX);
+        
+        // setting driver propertise
+        RemoteWebDriver driver = new RemoteWebDriver(new URL(nodeURL),cap);            
+        // Open URL
+        driver.get("https://registry.verra.org/mymodule/mypage.asp?p=login");
+        
+        // Enter Email id
+        driver.findElement(By.cssSelector("#myuserid")).sendKeys("Innovobot");
+
+        // Enter Password
+        driver.findElement(By.cssSelector("#mypassword")).sendKeys("CarbonCredits123$");
+        
+        //Click on login button
+        driver.findElement(By.cssSelector(".btn")).click();
+       
+        //driver.manage().timeouts().implicitlyWait(7,TimeUnit.SECONDS);
+        
+        // Open Sub-Account
+        driver.findElement(By.cssSelector("tr:nth-child(2) > td:nth-child(5) a")).click();
+        
+        // choose Active Sub-Accounts
+        driver.findElement(By.cssSelector("#id")).click();
+        driver.findElement(By.id("All")).click();
+        
+        // Download Active Sub-Accounts report
+        driver.findElement(By.cssSelector(".mr-1:nth-child(2) > img")).click();
+        
+        // Return to Home
+        driver.findElement(By.cssSelector(".navbar > .text-primary")).click();
+        
+        //Wait to load
+        //driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+        
+        //upload csv
+        //we want to import file. 
+        //String filePath = 
+        //System.getProperty("user.dir") + "/src/res/test.pdf
+        //driver.findElement(By.name("temp (5).csv")).sendKeys("C:\\Users\\TandraTech\\Downloads\\temp (5).csv");
+
+        //refresh the page
+        //driver.navigate().refresh();
     }
 }
+
+ 
